12d718085c2217aa8b48ee7b4b1e53007f657e86
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-09-05 19:19:53 -0300

    Probando fetch.

--- src/helpers/ComicVine/getComic.js
+++ src/helpers/ComicVine/getComic.js
@@ -1,7 +1,7 @@
 import { apiKey } from "./apiKey";
 
 export function getComic(id) {
-    return fetch(`https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&filter=id:${id}`)
+    return fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&filter=id:${id}`)
         .then((res)=> res.json())
         .then((data)=> {
             let {results : [comic]} = data;
--- src/helpers/ComicVine/getListComics.js
+++ src/helpers/ComicVine/getListComics.js
@@ -1,7 +1,7 @@
 import { apiKey } from "./apiKey";
 
 export function getListComics(categoryId) {
-    return fetch(`https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&sort=cover_date:desc&filter=volume:${categoryId}&limit=9`)
+    return fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&sort=cover_date:desc&filter=volume:${categoryId}&limit=9`)
         .then((res)=> res.json())
         .then((data)=> {
             let {results : comics} = data
