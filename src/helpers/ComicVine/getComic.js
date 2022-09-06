b8d9873bf465d3374b4ccc16eb9f7b60d62b8193
Ezequiel Dutra <squiel.dutra@gmail.com>
2022-09-06 00:31:11 -0300

    Corrigiendo CORS.

--- src/helpers/ComicVine/getComic.js
+++ src/helpers/ComicVine/getComic.js
@@ -1,10 +1,20 @@
 import { apiKey } from "./apiKey";
 
 export function getComic(id) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&filter=id:${id}`)
+    /*return fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&filter=id:${id}`)
         .then((res)=> res.json())
         .then((data)=> {
             let {results : [comic]} = data;
             return comic;
-        });
+        });*/
+
+    return fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&filter=id:${id}`)}`)
+    .then(response => {
+        if (response.ok) return response.json()
+        throw new Error('Network response was not ok.')
+    })
+    .then(data => {
+        let {results : [comic]} = data;
+        return comic;
+    });
 }
--- src/helpers/ComicVine/getListComics.js
+++ src/helpers/ComicVine/getListComics.js
@@ -1,10 +1,28 @@
 import { apiKey } from "./apiKey";
+import { XMLParser, XMLBuilder, XMLValidator} from "fast-xml-parser"
+
 
 export function getListComics(categoryId) {
-    return fetch(`https://cors-anywhere.herokuapp.com/https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&sort=cover_date:desc&filter=volume:${categoryId}&limit=9`)
+    /*return fetch(`https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&sort=cover_date:desc&filter=volume:${categoryId}&limit=9`)
         .then((res)=> res.json())
         .then((data)=> {
             let {results : comics} = data
+            console.log(data)
             return comics ;
-        });
+        });*/
+
+
+
+// with fetch
+return fetch(`https://api.allorigins.win/raw?url=${encodeURIComponent(`https://comicvine.gamespot.com/api/issues/?api_key=${apiKey}&format=json&sort=cover_date:desc&filter=volume:${categoryId}&limit=9`)}`)
+.then(response => {
+	if (response.ok) return response.json()
+	throw new Error('Network response was not ok.')
+})
+.then(data => {
+    const parser = new XMLParser();
+    console.log(data.results)
+    let {results : comics} = data
+            return comics ;
+});
 }
